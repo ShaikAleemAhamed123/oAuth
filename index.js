83d57d82e420af09adbadc7775dfdcbc1c9703b1
Aleem Ahamed <shaikaleemahamed786@gmail.com>
2022-06-20 20:13:14 +0530

    added req,res in "/" get path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const app=express();
 
-app.get("/",()=>{
-    res.send("<div>Hello World !</div>");
+app.get("/",(req,res)=>{
+    res.send("Hello World !");
 })
 
 
